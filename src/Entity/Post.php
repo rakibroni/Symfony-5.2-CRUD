8ab1ceb2840f97f5edae76055be6c87fb0dbe47d
Rakib Roni <rakibronicse@gmail.com>
2021-01-25 01:34:12 +0100

    Symfony CRUD application

---
+++ src/Entity/Post.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=99999)
+     */
+    private $description;
+
+
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true)
+     */
+    private $post_test;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+
+    public function getPostTest(): ?string
+    {
+        return $this->post_test;
+    }
+
+    public function setPostTest(?string $post_test): self
+    {
+        $this->post_test = $post_test;
+
+        return $this;
+    }
+}
